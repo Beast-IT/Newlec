cd6fc8bc53d707b1c9f172fe6d109697061cdb59
JJIN <tone231@naver.com>
2023-06-15 15:41:07 +0900

    별찍기 피라미드

--- 방과후연습용/src/방과후연습용/Jin/Star.java
+++ 방과후연습용/src/방과후연습용/Jin/Star.java
@@ -4,18 +4,20 @@ public class Star {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 4; i++) {
 
-			for (int j = 0; j < 5 - (i + 1); j++) {
+			for (int j = 0; j < 3 - i; j++) {
 				System.out.printf(" ");
 
 			}
 
-			for (int j = 0; j < i + 1; j++)
+			for (int k = 0; k < 1 + (i * 2); k++)
 				System.out.printf("*");
 
 			System.out.println();
 
+			// i 0 1 2 3
+			// k 1 3 5 7
 		}
 	}
 }
