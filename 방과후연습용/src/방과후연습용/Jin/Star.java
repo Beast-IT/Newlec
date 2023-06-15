81ccd9e461e348017385a3460cf2cad483c28558
JJIN <tone231@naver.com>
2023-06-15 14:12:32 +0900

    별찍기1

--- 방과후연습용/src/방과후연습용/Jin/Star.java
+++ 방과후연습용/src/방과후연습용/Jin/Star.java
@@ -3,8 +3,14 @@ package 방과후연습용.Jin;
 public class Star {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		for (int i = 0; i < 5; i++) {
+			
+			for (int j = 0; j < (i+1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
 	}
-
 }
