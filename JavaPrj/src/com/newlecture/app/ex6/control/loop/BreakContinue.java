4eaf8ea5016de589e5eeead45432e6f2ab4b9ce2
JJIN <tone231@naver.com>
2023-06-15 11:07:00 +0900

    문제3) 파일에서 성적을 읽다가 100점을 만나면 그 이후 모두 출력

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/BreakContinue.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/BreakContinue.java
@@ -8,8 +8,7 @@ import java.nio.charset.Charset;
 import java.util.Scanner;
 
 //break와continue 문제 
-//준비사항) 
-//res/exam.list 파일에 국어 성적을 다음처럼 저장한다.
+//준비사항) res/exam.list 파일에 국어 성적을 다음처럼 저장한다.
 //30 49 38 49 50 80 90 100 70 50 60 74 50 33 50 65 76 69
 
 //문제 0)파일에서 모든 성적을 읽어서 다음처럼 화면에 출력하시오
@@ -21,44 +20,38 @@ import java.util.Scanner;
 //문제 2) 파일에서 성적을 5번부터 읽어서 다음처럼 화면에 출력하시오.
 //50, 80, 90, 100, 70, 50, 60, 74, 50, 33, 50, 65, 76, 69,
 
+//문제 3) 파일에서 성적을 읽다가 100점을 만나면 그 이후의 성적을 모두 출력하시오. 
+
 public class BreakContinue {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		FileInputStream fis = new FileInputStream("res/exam.list");
 		Scanner fscan = new Scanner(fis);
-		
-		FileOutputStream fos = new FileOutputStream("res/exam_out.list");
-		PrintWriter fout = new PrintWriter(fos, true, Charset.forName("UTF-8"));
-		
-		
-		
-		
-		for(int i=0;fscan.hasNext();i++)
-		{
-			
-				
-			String word =fscan.next();	
-			int kor = Integer.valueOf(word);	
-			
-			//문제2)  i가 0(1번째) 1(2번째) 2(3번째) 3(4번째) 읽어오고 콘솔에 출력을 안하고 날려버린다. Continue로 밑에 출력을 쌩깐다.
-			if(i<4) 
-				continue;
-			
-			System.out.printf("%d, ",kor);//콘솔에 출력하기.
-			fout.printf("%d, ",kor);//파일에 출력하기.
-			
 
+		FileOutputStream fos = new FileOutputStream("res/exam_out.list");
+
+		boolean print = false;
+
+		for (int i = 0; fscan.hasNext(); i++) {
+
+			String word = fscan.next();
+			int kor = Integer.valueOf(word);
+
+			//문제 3) 파일에서 성적을 읽다가 100점을 만나면 그 이후의 성적을 모두 출력하시오.
+			if (kor == 100)
+				print = true;
 			
+			if (print)
+				System.out.printf("%d, ", kor);// 콘솔에 출력하기.
+
 		}
-		
-		
-		fout.close();
+
 		fos.close();
-		
+
 		fscan.close();
 		fis.close();
-		
+
 	}
 
 }
