9c41ae037d0baba7cc45dadb982597e678984edb
JJIN <tone231@naver.com>
2023-06-16 16:31:26 +0900

    for문 구구단

--- 방과후연습용/src/방과후연습용/Jin/Ex_0616.java
+++ 방과후연습용/src/방과후연습용/Jin/Ex_0616.java
@@ -1,27 +1,19 @@
 package 방과후연습용.Jin;
-//✏️  while문을 이용하여, number가 짝수마다 +가 출력되고, 홀수마다 -가 출력되는 코드를 작성해보세요. 이때, x축의 문자가 10개가 되면, 줄바꿈을 해보세요
 
-//-+-+-+-+-+
-//-+-+-+-+-+
-//-+-+-+-+-+
-//…(반복)
-
+//✏️for문을 이용하여  다음처럼 구구단 표를 출력해보세요.
+//2 * 1 = 2
+//2 * 2 = 4
+//.
+//9 * 9 = 81
 public class Ex_0616 {
 	public static void main(String[] args) {
-		int i = 0;
-		int j = 0;
-
-		while (i < 10) {
-			while (j < 10) {
-				if ((j + 1) % 2 == 0)
-					System.out.print("+");
-				else
-					System.out.print("-");
-				j++;
+		for (int i = 0; i < 9; i++) {
+			if(i==8)
+				break;
+			for (int j = 0; j < 9; j++) {
+					System.out.printf("%d * %d = %d\n", i + 2, j + 1, (i + 2) * (j + 1));
 			}
 			System.out.println();
-			j = 0;
-			i++;
 		}
-	}
+	}// end main
 }
