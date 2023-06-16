d8b6aded9b95d516c6ea66d9e086c28def460ed7
JJIN <tone231@naver.com>
2023-06-16 16:11:48 +0900

    이중 while문

--- 방과후연습용/src/방과후연습용/Jin/Ex_0616.java
+++ 방과후연습용/src/방과후연습용/Jin/Ex_0616.java
@@ -1,17 +1,27 @@
 package 방과후연습용.Jin;
-//✏️  while문을 이용하여, number가 짝수마다 +가 출력되고, 홀수마다 -가 출력되는 코드를 작성해보세요.
+//✏️  while문을 이용하여, number가 짝수마다 +가 출력되고, 홀수마다 -가 출력되는 코드를 작성해보세요. 이때, x축의 문자가 10개가 되면, 줄바꿈을 해보세요
 
-//		number의 값이 100이 되면 while문을 빠져나와주세요.
-//		-+-+-+-+-+... (반복)
+//-+-+-+-+-+
+//-+-+-+-+-+
+//-+-+-+-+-+
+//…(반복)
+
 public class Ex_0616 {
 	public static void main(String[] args) {
-		int number = 1;
-		while (number <= 100) {
-			if ((number % 2) == 0)
-				System.out.print("+");
-			else
-				System.out.print("-");
-			number++;
+		int i = 0;
+		int j = 0;
+
+		while (i < 10) {
+			while (j < 10) {
+				if ((j + 1) % 2 == 0)
+					System.out.print("+");
+				else
+					System.out.print("-");
+				j++;
+			}
+			System.out.println();
+			j = 0;
+			i++;
 		}
 	}
 }
