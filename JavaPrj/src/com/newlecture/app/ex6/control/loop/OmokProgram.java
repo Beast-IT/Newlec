a136c693ff3596c49a9312aa91ce3163641748ec
JJIN <tone231@naver.com>
2023-06-16 14:25:01 +0900

    오목판 완성v1.1

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/OmokProgram.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/OmokProgram.java
@@ -6,15 +6,15 @@ public class OmokProgram {
 
 		for (int x = 0; x < 12; x++) {
 			for (int y = 0; y < 10; y++)
-				 if (x == 0 && y == 9)// 오른쪽 위 모서리막기
-					System.out.print("┐");
-				else if (x == 11 && y == 9)// 오른쪽 아래 모서리막기
-					System.out.print("┘");
-				else if (x == 0 && y == 0)// 왼쪽 위 모서리막기
+				if (x == 0 && y == 0)// 왼쪽 위 모서리막기
 					System.out.print("┌");
+				else if (x == 0 && y == 9)// 오른쪽 위 모서리막기
+					System.out.print("┐");
 				else if (x == 11 && y == 0)// 왼쪽 아래 모서리막기
 					System.out.print("└");
-				else	if (x == 0)// 위에 막기
+				else if (x == 11 && y == 9)// 오른쪽 아래 모서리막기
+					System.out.print("┘");
+				else if (x == 0)// 위에 막기
 					System.out.print("┬");
 				else if (x == 11)// 아래 막기
 					System.out.print("┴");
