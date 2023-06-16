d5f524b082eab4cb506ec3af6ca4dbe0afca4125
JJIN <tone231@naver.com>
2023-06-16 12:17:10 +0900

    else if문

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/Doublefor.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/Doublefor.java
@@ -4,12 +4,20 @@ public class Doublefor {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 6; i++) {
-			
-			for (int j = 0; j < 10-i; j++) {
-				System.out.printf("%d ", j + 1);
+		for (int j = 0; j < 10; j++) {
+			for (int i = 0; i < 10; i++) {
+
+				if (i == 2)
+					System.out.print("a ");
+				else if (i == 5)
+					System.out.print("b ");
+				else if (i==9)
+					System.out.print("c ");
+				else
+					System.out.printf("%d ", i + 1);
+
 			}
-			
+
 			System.out.println();
 		}
 	}
