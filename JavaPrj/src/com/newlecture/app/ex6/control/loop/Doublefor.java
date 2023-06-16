ea16a40ea7cedef0bf89c025dd419592d82eb839
JJIN <tone231@naver.com>
2023-06-16 11:00:59 +0900

    이중 for문 1

---
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/Doublefor.java
@@ -0,0 +1,15 @@
+package com.newlecture.app.ex6.control.loop;
+
+public class Doublefor {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("%d ", j + 1);
+			}
+			System.out.println();
+		}
+	}
+
+}
