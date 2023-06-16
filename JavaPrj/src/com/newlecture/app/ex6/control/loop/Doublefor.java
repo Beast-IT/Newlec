23aa2268ec845ab5b7c5a40e878c2e2f140f4379
JJIN <tone231@naver.com>
2023-06-16 11:47:10 +0900

    이중 for문 4

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/Doublefor.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/Doublefor.java
@@ -4,9 +4,9 @@ public class Doublefor {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 6; i++) {
 			
-			for (int j = 0; j < i+3; j++) {
+			for (int j = 0; j < 10-i; j++) {
 				System.out.printf("%d ", j + 1);
 			}
 			
