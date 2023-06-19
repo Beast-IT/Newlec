f833508c33ac49bf86bc9699c436788e73e8f54c
Jinseunghyun <tone231@naver.com>
2023-06-19 11:34:33 +0900

    오목판 그래프 채우기

--- JavaPrj/src/com/newlecture/app/ex6/control/loop/OmokProgram.java
+++ JavaPrj/src/com/newlecture/app/ex6/control/loop/OmokProgram.java
@@ -4,26 +4,28 @@ public class OmokProgram {
 
 	public static void main(String[] args) {
 		/*
-		 * 문제: 오목판에 오른쪽 X모으로 흰색 바둑돌을 넣어라.
+		 * 문제: 오목판에 화살표 모양의 흰색 바둑돌을 넣어라.
 		 * 
 		 * 
 		 * 
 		 * 
 		 * 
 		 * 문제푼날: 230619 
-		 * 풀이: 나 
-		 * 핵심: (1,7) (2,6) (3,5)...(7,1) 좌표의 합이 8로 같다! 
+		 * 풀이: 선생님  
+		 * 핵심: 일차함수 그래프를 생각하자 ! 
 		 */
 		for (int x = 0; x < 12; x++) {
 			for (int y = 0; y < 12; y++)
+				
 				if (x == 0 && y == 0)// 왼쪽 위 모서리막기
 					System.out.print("┌");
-				else if (x == 0 && y == 11)// 오른쪽 위 모서리막기
-					System.out.print("┐");
-				else if (x == 11 && y == 0)// 왼쪽 아래 모서리막기
-					System.out.print("└");
 				else if (x == 11 && y == 11)// 오른쪽 아래 모서리막기
 					System.out.print("┘");
+				else if (x == 11 && y == 0)// 왼쪽 아래 모서리막기
+					System.out.print("└");
+				else if (x == 0 && y == 11)// 오른쪽 위 모서리막기
+					System.out.print("┐");
+			
 				else if (x == 0)// 위에 막기
 					System.out.print("┬");
 				else if (x == 11)// 아래 막기
@@ -32,8 +34,13 @@ public class OmokProgram {
 					System.out.print("├");
 				else if (y == 11)// 오른쪽 막기
 					System.out.print("┤");
-				else if (x+y==8)
+				
+				
+				else if (x == y-2 || y == -x+8) // 그래프 y-x, x-y
 					System.out.print("●");
+				else if (x >= y-2 && y <= -x+8)// 영역안에 채우기
+					System.out.print("●");
+			
 				else
 					System.out.print("┼");
 
