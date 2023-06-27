9b548c7500af1f944926236c9f6db9f0cb96384f
Jinseunghyun <tone231@naver.com>
2023-06-27 11:46:45 +0900

    메서드(함수) 만들어보기

--- JavaPrj/src/com/newlecture/app/ex8/function/BasicProgram2.java
+++ JavaPrj/src/com/newlecture/app/ex8/function/BasicProgram2.java
@@ -5,10 +5,18 @@ public class BasicProgram2 {
        int total=(n*(n+1))/2;
         return total;
     }
+    static int power(int x){
+        int total =((x+3)*(x+3)+ x/3*(x-2)+5);
+        return total;
+    }
     public static void main(String[] args) {
-//        int result = 1+2+3+4+5+6+7+8+9+10~n;
-        int result=sum(73738);
-
+        //int result = 1+2+3+4+5+6+7+8+9+10~n;
+        int result = sum(11);
         System.out.printf("result : %d\n",result);
+
+        //(x+3)*(x+3)+ x/3*(x-2)+5
+        int result2 = power(7)+3+power(7)-power(3);
+        System.out.printf("result : %d\n",result2);
+
     }
 }
