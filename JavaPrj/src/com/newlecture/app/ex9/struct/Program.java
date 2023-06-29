234a91954c1745ce4e0c417f91de9b972b7545fc
Jinseunghyun <tone231@naver.com>
2023-06-29 11:59:50 +0900

    객체배열

--- JavaPrj/src/com/newlecture/app/ex9/struct/Program.java
+++ JavaPrj/src/com/newlecture/app/ex9/struct/Program.java
@@ -15,29 +15,38 @@ public class Program {
         int math;
 
         //성적 로드
+        Exam[] exams = new Exam[20];
         {
             FileInputStream fis = new FileInputStream("JavaPrj/res/exam.data");
             Scanner fscan = new Scanner(fis);
 
             fscan.nextLine();
 
-            Exam exam = new Exam();
-            String line = fscan.nextLine(); //"80,90,95"
-            String[] tokens =line.split(",");//"80,90,95" -> "80" "90" "95"
+            int index=0;
+            while(fscan.hasNextLine()) {
+                Exam exam = new Exam();
 
-            exam.kor=Integer.parseInt(tokens[0]);
-            exam.eng=Integer.parseInt(tokens[1]);
-            exam.math=Integer.parseInt(tokens[2]);
+                String line = fscan.nextLine(); //"80,90,95"
+                String[] tokens = line.split(",");//"80,90,95" -> "80" "90" "95"
 
+                exam.kor = Integer.parseInt(tokens[0]);
+                exam.eng = Integer.parseInt(tokens[1]);
+                exam.math = Integer.parseInt(tokens[2]);
+
+                exams[index] = exam;
+                index++;
+            }
             fscan.close();
             fis.close();
 
-            System.out.println(exam.kor);
-            System.out.println(exam.eng);
-            System.out.println(exam.math);
-
-
-        }
-
+            //성적 출력
+            {
+                for(int i=0;i< index;i++) {
+                        System.out.printf("%d ", exams[i].kor);
+                        System.out.printf("%d ", exams[i].eng);
+                        System.out.printf("%d\n", exams[i].math);
+                }
+            }
+        }// 성적 로드
     }//end main
 }//end class
