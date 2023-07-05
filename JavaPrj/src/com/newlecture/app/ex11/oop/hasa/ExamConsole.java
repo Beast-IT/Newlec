f3891adcc47a54a1cf1bb0e8f98f5aa73311a919
Jinseunghyun <tone231@naver.com>
2023-07-05 16:29:19 +0900

    has a상속

---
+++ JavaPrj/src/com/newlecture/app/ex11/oop/hasa/ExamConsole.java
@@ -0,0 +1,40 @@
+package com.newlecture.app.ex11.oop.hasa;
+
+import java.util.Scanner;
+
+public class ExamConsole {
+    private Exam exam = new Exam();// has a 상속관계/ A(ExamConsole)가 B(Exam)를 가지고 있다.
+
+    public void input() {
+        Scanner scan = new Scanner(System.in);
+
+        int kor = exam.getKor();
+        int eng = exam.getEng();
+        int math = exam.getMath();
+
+        System.out.print("kor : ");
+        kor = scan.nextInt();
+
+        System.out.print("eng : ");
+        eng = scan.nextInt();
+
+        System.out.print("math : ");
+        math = scan.nextInt();
+    }
+
+    public void print() {
+
+        exam.setKor(100);
+        exam.setEng(90);
+        exam.setMath(50);
+
+        int kor = exam.getKor();
+        int eng = exam.getEng();
+        int math = exam.getMath();
+        System.out.printf("kor : %d, eng : %d, math : %d\n", kor, eng, math);
+        System.out.printf("total : %d\n", exam.total());
+        System.out.printf("avg %.2f\n", exam.avg());
+
+    }
+
+}
---
+++ JavaPrj/src/com/newlecture/app/ex11/oop/hasa/Program.java
@@ -0,0 +1,35 @@
+package com.newlecture.app.ex11.oop.hasa;
+
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) throws IOException {
+        ExamConsole console = new ExamConsole();
+
+        console.input();
+        console.print();
+
+
+
+
+
+
+
+//exam.kor=4;//private을 사용해서 캡슐화가 깨지는것 방지.
+//        exam.aa = 20;//잘못된 static 접근 방법
+//        Exam.aa = 100;//올바른 static 접근 방법
+//        System.out.printf("전역 변수 : %d\n", Exam.aa);
+
+
+//        Exam.input(exam);//static Method
+//        System.out.printf("Static Method end : %d\n", exam.eng);
+//
+//        exam.input();//instance Method
+//        System.out.printf("Instance Method eng : %d\n", exam.eng);
+//        exam.print();
+
+//        System.out.println();
+
+    }
+
+}
